cb02e4d95c03e8ecc8025b98200134dc9fc05db3
Darvinesha <25089558@imail.sunway.edu.my>
2025-07-21 06:03:45 +0000

    Work done for day 1

--- Part1/ex08.py
+++ Part1/ex08.py
@@ -1,6 +1,7 @@
 # Current speed
 speed = 61
 print(speed)
-# If the speed is more than 60,
-if speed > 61
-print(speed['Over the speed limit'])
+
+# Check if the speed is more than 60,
+if speed > 60:
+ print('Over the speed limit')
